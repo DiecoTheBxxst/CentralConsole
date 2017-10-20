988a9b5e9cf6679d5eb6e97f629f4df9b9736e38
Diego Toniolo <diego.toniolo@outlook.it>
2017-10-21 00:14:32 +0200

    Delete cc-webhome.sh

--- System/cc-webhome.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-[ -r /etc/default/cc-webhome ] && . /etc/default/cc-webhome
-
-cd /opt/CentralConsole/Script/Web
-
-/opt/CentralConsole/jruby/bin/jruby home.rb >> /var/log/CentralConsole/WebService.log 2>&1
-
